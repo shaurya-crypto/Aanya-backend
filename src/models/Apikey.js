19a789ad04907203f9b286f88d5f2622c5670e03
shaurya-crypto <shauryaprabhakar097@gmail.com>
2026-02-11 11:23:12 +0530

    Update Apikey.js

--- src/models/Apikey.js
+++ src/models/Apikey.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const apiKeySchema = new mongoose.Schema({
   key: String,
+  key: { type: String, required: true, unique: true },
   userId: mongoose.Schema.Types.ObjectId,
   active: { type: Boolean, default: true },
   createdAt: { type: Date, default: Date.now },
 
   // --- RATE LIMITING FIELDS ---
   requestCount: { type: Number, default: 0 },
-  lastRequestDate: { type: String, default: null } // Stores "YYYY-MM-DD"
+  lastRequestDate: { type: String, default: null } // Stores "YYYY-MM-DD" 1
 });
 
 export default mongoose.model("ApiKey", apiKeySchema);
