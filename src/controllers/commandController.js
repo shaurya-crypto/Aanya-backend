9f73aaf1cf72d3908fbb69d02838f7e45bb8cb2d
Shaurya <shauryaprabhakar097@gmail.com>
2026-09-10 10:32:54 +0530

    Updated new Model for working

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -220,7 +220,7 @@ const messages = [
       const client = new Groq({ apiKey: key });
       return await client.chat.completions.create({
         messages: messages,
-        model: "llama-3.1-8b-instant",
+        model: "openai/gpt-oss-20",
         temperature: 0.6,
         response_format: { type: "json_object" } // Strictly locked to JSON
       });
